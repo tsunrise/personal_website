54f52a39647d37ef46d7a9d448615d75f29af35b
Tom Shen <me@tomshen.io>
2022-07-08 20:54:58 -0700

    update info (#67)

---
+++ src/data/background.tsx
@@ -0,0 +1,50 @@
+import { Code, Computer, School, Work } from "@material-ui/icons"
+
+interface BackgroundEntry {
+    time: string | number,
+    icon: JSX.Element,
+    icon_color?: "inherit" | "grey" | "primary" | "secondary" | undefined,
+    header: string,
+    main: string,
+    end?: boolean
+}
+
+export const entries: BackgroundEntry[] = [
+    {
+        time: `2018`,
+        icon: <School />,
+        icon_color: "primary",
+        header: `Computer Science BA (2018-2022)`,
+        main: `UC Berkeley`,
+    },
+    {
+        time: `2020`,
+        icon: <Computer />,
+        icon_color: "primary",
+        header: `Undergraduate Research`,
+        main: `arkworks & RISELab`,
+    },
+    {
+        time: `2021`,
+        icon: <Work />,
+        icon_color: "primary",
+        header: `Intern (Backend)`,
+        main: `Arista`,
+    },
+    {
+        time: `2021`,
+        icon: <Code />,
+        icon_color: "primary",
+        header: `Independent Open Source Contributor`,
+        main: `Manta Network`,
+    },
+    {
+        time: `2022`,
+        icon: <School />,
+        icon_color: "primary",
+        header: `MSCS`,
+        main: `Stanford`,
+        end: true
+    },
+
+]
