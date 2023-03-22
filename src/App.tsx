0b0424737b884aeffaeba3fee4bd21461874f98c
Conghao Shen <me@tomshen.io>
2023-03-22 15:25:30 -0700

    start working on v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,25 @@
-import React, {useEffect} from 'react';
-import MainGrid from "./components/main";
-import '@fontsource/roboto';
-import {ThemeProvider} from '@material-ui/styles';
-import {createMuiTheme} from "@material-ui/core";
-import {grey} from "@material-ui/core/colors";
-
-const theme = createMuiTheme({
-    palette: {
-        primary: {
-            light: `#4f83b5`,
-            main: `#155c9e`,
-            dark: `#004e96`,
-            contrastText: `#fff`
-        },
-        secondary: {
-            light: grey[600],
-            main: grey[700],
-            dark: grey[800],
-            contrastText: `#fff`
-        }
-    }
-})
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    useEffect(() => {
-        console.log("%cWelcome to Tom Shen's Personal Website. \nMy Github: https://github.com/tsunrise",
-            `color: white; font-size: 15px`);
-        document.title = "Tom Shen"
-    }, [])
-
-    return (
-        <div className="App">
-            <ThemeProvider theme={theme}>
-                <MainGrid/>
-            </ThemeProvider>
-        </div>
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/data/introduction_roles.tsx
+++
@@ -1,7 +0,0 @@
-export const introductionRoles = [
-    "a student",
-    "a software engineer",
-    "a researcher",
-    "an open source committer",
-    "an anime fans"
-]
